acb469af82d23bbf7db1cab9d6b78cd51c67cd49
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-10-18 14:20:10 +0100

    fixture test added for key value set

--- spec/fixtures/app_spec.rb
+++ spec/fixtures/app_spec.rb
@@ -1,6 +1,6 @@
 ENV['RACK_ENV'] = 'test'
 
-require 'app.rb'
+require 'spec_helper'
 require 'test/unit'
 require 'rack/test'
 
@@ -8,6 +8,14 @@ class DatabaseServerTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
   def app
-     Sinatra::Application
+     DatabaseServer.new
   end
+
+  def test_it_can_set_key_value
+    get '/set?key=value'
+    assert last_response.ok?
+    assert_equal 'key, value', last_response.body
+  end
+
+
 end
